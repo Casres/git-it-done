fccb8d39bf71cecb41d83ade2ca86ec0a01fbcab
Casres <christian@casillas.tech>
2022-03-31 17:53:28 -0400

    able to pull repos from user name via the users input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,26 @@
 
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var getUserNameBtn = document.querySelector("#get-user-btn");
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    
+    // gets the value from the users input
+    var username = nameInputEl.value.trim();
 
+    if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+    alert("Please enter a GitHub username");
+    }
+};
+
+// getUserNameBtn.addEventListener("click", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
+ 
 var getUserRepos = function (user) {
   // format the GitHub API URL
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -14,4 +33,4 @@ var getUserRepos = function (user) {
   });
 };
 
-getUserRepos("Casres");
+getUserRepos();
